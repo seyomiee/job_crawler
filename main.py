ec7a392a248d1f9635c0b397bdb7a426fb9f1f52
seyeon <tltptltptltp@gmail.com>
2020-04-26 20:57:21 +0900

    main html,py

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ https://stackoverflow.com/jobs?r=true&q=python
 https://weworkremotely.com/remote-jobs/search?term=python
 https://remoteok.io/remote-dev+python-jobs
 """
+import csv
+import requests
+from flask import Flask, render_template, request, Response 
+from bs4 import BeautifulSoup 
 
-def
+
+app = Flask('jobs')
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+app.run(host="0.0.0.0", debug=True)
